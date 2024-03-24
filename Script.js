21ef30cee5957204d00a288f84e4fcfb2fba6a82
Ciposki <74446443+Ciposki@users.noreply.github.com>
2024-03-24 10:59:42 +0100

    AAAAAAAA JAvACScad fm fc s G JNSE

---
+++ Script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const Button1 = document.querySelector(".button1");
+    const bg = document.querySelector(".background");
+    const gamesect= document.querySelector(".gameSection")
+    Button1.addEventListener("mouseover", () => {
+        if(!bg.classList.contains("Hide")){
+            bg.classList.add("Hide")
+            gamesect.classList.remove("Hide")
+            console.log("Hover");
+        }
+        
+    });
+
+    Button1.addEventListener("mouseout", () => {
+        if(bg.classList.contains("Hide")){
+            bg.classList.remove("Hide")
+            gamesect.classList.add("Hide")
+        }
+            
+    });
+});
