eb66c58ab369f2502aae8511a22f2574b9917da2
Ciposki <74446443+Ciposki@users.noreply.github.com>
2024-03-25 17:51:32 +0100

    NON SONO A SUCIDARM GIURO

--- Script.js
+++ Script.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const Button1 = document.querySelector(".button1");
+    
     const bg = document.querySelector(".background");
     const gamesect= document.querySelector(".gameSection")
     Button1.addEventListener("mouseover", () => {
