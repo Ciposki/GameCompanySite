e1f3d9feb0edd9fd9c4fa25642a126d1ef2ca780
“GIULIOHHH” <iosonogiulioareste@gmail.com>
2024-03-30 15:46:37 +0100

    mbare
    mbrae

--- Gameinfo.js
+++ Gameinfo.js
@@ -27,20 +27,55 @@ async function getdata(){
         }
     }
 }
+
+function load(){
+    let download_counter=document.getElementById("download_counter")
+    let view_counter=document.getElementById("view_counter")
+    let download_bar= document.getElementById("download_bar");
+    let view_bar= document.getElementById("view_bar");
+    setdata();
+    const cursor= document.querySelector('.cursor');
+    let flashlightable= document.querySelector('.flashlightable');
+    let mouseX=0;
+    let mouseY=0;
+    const bro=document.getElementById('bro');
+    const positionElement = (e)=> {
+
+        mouseY = e.clientY;
+        mouseX = e.clientX;
+
+        cursor.style.transform = `translate3d(${mouseX-10}px, ${mouseY-5}px, 0)`;
+        //if stuff
+
+
+        flashlightable.style.webkitMaskImage = `radial-gradient(circle at ${mouseX/window.innerWidth*100}% ${mouseY/window.innerHeight*100}%, black -10%, transparent 30%)`;
+
+// And set the mask-image property
+        flashlightable.style.maskImage = `radial-gradient(circle at ${mouseX/window.innerWidth*100}% ${mouseY/window.innerHeight*100}%, black 0%, transparent 40%)`;
+    }
+
+
+    function removebro(){
+        bro.classList.add("animate_adios")
+        bro.opacity="0";
+    }
+    window.addEventListener('mousemove', positionElement);
+    bro.addEventListener("mouseover",removebro)
+}
+
 function setdata(){
     getdata().then(
         function(data){
 
-            let download_counter=document.getElementById("download_counter")
+            //this somehow works
             download_counter.textContent=data[0];
-            let view_counter=document.getElementById("view_counter")
+
             view_counter.textContent=data[1];
             //max downloads
             let downloadbound=100;
             //max views
             let view_bound=400;
-            let download_bar= document.getElementById("download_bar");
-            let view_bar= document.getElementById("view_bar");
+
             let ratio=(data[0]/downloadbound)*90;
             download_bar.style.width=ratio.toString()+"vh";
             ratio=(data[1]/view_bound)*90;
@@ -49,11 +84,9 @@ function setdata(){
         }
     )
 }
+
 //after the page loads
-document.addEventListener('DOMContentLoaded',setdata);
+document.addEventListener('DOMContentLoaded',load);
 //every 2 seconds
 setInterval(setdata, 2000);
-function set_flash(){
-    const cursor= document.querySelector('.cursor');
 
-}
